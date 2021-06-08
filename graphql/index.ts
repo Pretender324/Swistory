afc1750765b1227ffec63c01bf62113a37de1377
Pretender324 <r1kuchicken5761@gmail.com>
2021-06-08 17:33:57 +0900

    graphql 接続

--- graphql/index.ts
+++ graphql/index.ts
@@ -1,7 +1,12 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+
+const link = new HttpLink({
+  uri: 'http://localhost:4000/graphql',
+  // Additional options
+})
 
 export const client = new ApolloClient({
   //TODO: productionに修正
-  uri: 'http://localhost:4000',
+  link,
   cache: new InMemoryCache(),
 })
